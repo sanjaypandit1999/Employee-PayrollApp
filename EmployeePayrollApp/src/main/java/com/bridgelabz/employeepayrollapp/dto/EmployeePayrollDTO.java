f80dcb7021ca161aabb8a632e5c7c305f4ada3f3
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-09 21:53:17 +0530

    ADDED: Lombok Library

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,8 +1,5 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
-import lombok.Data;
-
-@Data
 public class EmployeePayrollDTO {
 	public String empName;
 	public long salary;
@@ -11,27 +8,6 @@ public class EmployeePayrollDTO {
 		super();
 		this.empName = empName;
 		this.salary = salary;
-	}
-
-	public String getEmpName() {
-		return empName;
-	}
-
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-
-	public long getSalary() {
-		return salary;
-	}
-
-	public void setSalary(long salary) {
-		this.salary = salary;
-	}
-
-	@Override
-	public String toString() {
-		return "EmployeePayrollDto [empName=" + empName + ", salary=" + salary + "]";
 	}
 
 }
--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.Data;
+
+@Data
 public class ResponseDTO {
 	private String message;
 	private Object data;
@@ -7,22 +10,6 @@ public class ResponseDTO {
 	public ResponseDTO(String message, Object data) {
 		super();
 		this.message = message;
-		this.data = data;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
 		this.data = data;
 	}
 
